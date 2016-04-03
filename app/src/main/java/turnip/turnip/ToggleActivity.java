7f88c3dc7dd392fbff09fa8a6d8302797da15b6c
ivey <iveysaurrr@gmail.com>
2016-04-03 17:57:33 -0400

    Add text under toggle button

--- app/src/main/java/turnip/turnip/ToggleActivity.java
+++ app/src/main/java/turnip/turnip/ToggleActivity.java
@@ -1,11 +1,11 @@
 package turnip.turnip;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class ToggleActivity extends AppCompatActivity {
 
@@ -13,6 +13,19 @@ public class ToggleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toggle);
+        Switch s = (Switch) findViewById(R.id.turnipToggle);
+
+        final Context context = this;
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView readiness = (TextView) findViewById(R.id.readiness);
+                if (isChecked) {
+                    readiness.setText(context.getResources().getString(R.string.readyTurnip));
+                } else {
+                    readiness.setText(context.getResources().getString(R.string.notReadyTurnip));
+                }
+            }
+        });
     }
 
 }
