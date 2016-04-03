db20b141a7031c9be07af5f1c0788079a4c0607e
ivey <iveysaurrr@gmail.com>
2016-04-03 14:00:25 -0400

    Tried to make toggle, failed

--- app/src/main/java/turnip/turnip/MainActivity.java
+++ app/src/main/java/turnip/turnip/MainActivity.java
@@ -1,5 +1,6 @@
 package turnip.turnip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -37,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void signIn(View view) {
+        Intent i;
+
+        i = new Intent(this, ToggleActivity.class);
+        startActivity(i);
     }
 }
