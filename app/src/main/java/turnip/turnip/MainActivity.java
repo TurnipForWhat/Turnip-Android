6983856e1eb4f231d8b6cdf4fd21b586b805d8d5
Brittney Fry <Brittney Fry>
2016-04-03 14:49:40 -0400

    added Sign up

--- app/src/main/java/turnip/turnip/MainActivity.java
+++ app/src/main/java/turnip/turnip/MainActivity.java
@@ -1,5 +1,7 @@
 package turnip.turnip;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+        final Context context = this;
+
+
+        TextView signUp= (TextView) findViewById(R.id.signUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent= new Intent(context,SignUpActivity.class);
+                startActivity(intent);
             }
+
         });
     }
 
