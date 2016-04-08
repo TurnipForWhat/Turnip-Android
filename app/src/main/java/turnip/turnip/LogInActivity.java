60beb1274d50ec317fa7709613eacc1c67f875b5
ivey <iveysaurrr@gmail.com>
2016-04-07 23:48:05 -0400

    Login through api

--- app/src/main/java/turnip/turnip/LogInActivity.java
+++ app/src/main/java/turnip/turnip/LogInActivity.java
@@ -1,10 +1,12 @@
 package turnip.turnip;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,18 +20,49 @@ public class LogInActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.logInButton);
 
-        final Context context = this;
+        assert button != null;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email = (EditText) findViewById(R.id.email);
-                Log.i("email", email.getText().toString());
-                EditText password = (EditText) findViewById(R.id.password);
-                Log.i("password", password.getText().toString());
-                Intent intent = new Intent(context, ToggleActivity.class);
-                startActivity(intent);
+                EditText txtEmail = (EditText) findViewById(R.id.email);
+                EditText txtPassword = (EditText) findViewById(R.id.password);
+                assert txtEmail != null;
+                assert txtPassword != null;
+                String email = txtEmail.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                logIn(email, password);
             }
         });
     }
 
+    void logIn(final String email, final String password) {
+        final Activity act = this;
+
+        final Context context = this;
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                final Boolean success = API.login(email, password);
+                act.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (success) {
+                            Intent intent = new Intent(context, ToggleActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            new AlertDialog.Builder(act)
+                                .setTitle(R.string.couldNotLogIn)
+                                .setMessage(R.string.loginError)
+                                .setNeutralButton(R.string.okay, null)
+                                .show();
+                        }
+                    }
+                });
+                return null;
+            }
+
+        }.execute(null, null, null);
+    }
 }
