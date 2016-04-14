c722f8c86596a796280cb020d831f25a4e6dc7be
ivey <iveysaurrr@gmail.com>
2016-04-14 01:46:52 -0400

    Use enter button to search

--- app/src/main/java/turnip/turnip/SearchActivity.java
+++ app/src/main/java/turnip/turnip/SearchActivity.java
@@ -6,11 +6,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,9 +33,27 @@ public class SearchActivity extends AppCompatActivity {
                 EditText search = (EditText) findViewById(R.id.search_text);
                 InputMethodManager imm = (InputMethodManager)getSystemService(context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                assert search != null;
                 getResults(search.getText().toString());
             }
         });
+
+        EditText editText = (EditText) findViewById(R.id.search_text);
+        assert editText != null;
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    EditText search = (EditText) findViewById(R.id.search_text);
+                    assert search != null;
+                    getResults(search.getText().toString());
+                }
+                return false;
+            }
+        });
+    }
+
+    void search() {
     }
 
     void getResults(final String query) {
